337875ee6ab697f082cda398629d8c82b7609b07
sagar-pardhi <sgrpardhi007@gmail.com>
2018-12-04 12:18:57 +0530

    Modified Appbar

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -34,6 +34,37 @@ class _MovieListState extends State<MovieList> {
           )
         ],
       ),
+      body: new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new MovieTitle(mainColor),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MovieTitle extends StatelessWidget {
+  final Color mainColor;
+
+  const MovieTitle(this.mainColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      child: new Text(
+        'Top Rated',
+        style: new TextStyle(
+            fontSize: 40.0,
+            color: mainColor,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Arvo'),
+        textAlign: TextAlign.left,
+      ),
     );
   }
 }
