25b2f6c45b42f677ac27192bf91a0c409756dbf4
sagar-pardhi <sgrpardhi007@gmail.com>
2018-12-05 12:10:27 +0530

    Fetching the movies data and Displaying the data in a list

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:movie_listing_app/config.dart';
 
 class MovieList extends StatefulWidget {
   @override
@@ -6,10 +10,20 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
+  var movies;
   Color mainColor = const Color(0xff3C3261);
+
+  void getData() async {
+    var data = await getJson();
+
+    setState(() {
+      movies = data['results'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: new AppBar(
@@ -40,6 +54,17 @@ class _MovieListState extends State<MovieList> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             new MovieTitle(mainColor),
+            new Expanded(
+              child: new ListView.builder(
+                  itemCount: movies == null ? 0 : movies.length,
+                  itemBuilder: (context, i) {
+                    return new FlatButton(
+                      child: new MovieCell(movies, i),
+                      padding: const EdgeInsets.all(0.0),
+                      color: Colors.white,
+                    );
+                  }),
+            )
           ],
         ),
       ),
@@ -65,6 +90,86 @@ class MovieTitle extends StatelessWidget {
             fontFamily: 'Arvo'),
         textAlign: TextAlign.left,
       ),
+    );
+  }
+}
+
+Future<Map> getJson() async {
+  var url = 'http://api.themoviedb.org/3/discover/movie?api_key=' + apiKey;
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
+}
+
+class MovieCell extends StatelessWidget {
+  final movies;
+  final i;
+  Color mainColor = const Color(0xff3C3261);
+  var image_url = 'https://image.tmdb.org/t/p/w500/';
+  MovieCell(this.movies, this.i);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Row(
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: new Container(
+                margin: const EdgeInsets.all(16.0),
+                child: new Container(
+                  width: 70.0,
+                  height: 70.0,
+                ),
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(10.0),
+                  color: Colors.grey,
+                  image: new DecorationImage(
+                      image: new NetworkImage(
+                          image_url + movies[i]['poster_path']),
+                      fit: BoxFit.cover),
+                  boxShadow: [
+                    new BoxShadow(
+                        color: mainColor,
+                        blurRadius: 5.0,
+                        offset: new Offset(2.0, 5.0))
+                  ],
+                ),
+              ),
+            ),
+            new Expanded(
+                child: new Container(
+              margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+              child: new Column(
+                children: [
+                  new Text(
+                    movies[i]['title'],
+                    style: new TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Arvo',
+                        fontWeight: FontWeight.bold,
+                        color: mainColor),
+                  ),
+                  new Padding(padding: const EdgeInsets.all(2.0)),
+                  new Text(
+                    movies[i]['overview'],
+                    maxLines: 3,
+                    style: new TextStyle(
+                        color: const Color(0xff8785A4), fontFamily: 'Arvo'),
+                  )
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            )),
+          ],
+        ),
+        new Container(
+          width: 300.0,
+          height: 0.5,
+          color: const Color(0xD2D2E1ff),
+          margin: const EdgeInsets.all(16.0),
+        )
+      ],
     );
   }
 }
